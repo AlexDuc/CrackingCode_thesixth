e0e6eb8dd81bffc69136635da0fefde32a3441fc
AlexDuc <ducnt87@gmail.com>
2017-11-15 23:30:23 -0600

    set stack

--- js/index3.js
+++ js/index3.js
@@ -25,3 +25,29 @@ stack.prototype.pop = function() {
         return item
     }
 }
+setOfStack = function (limit) {
+    var stackSet = []
+    var stackLimit = limit
+}
+setOfStack.prototype.push = function (val) {
+    if(this.stackSet.length === 0 || this.stackSet[this.stackSet.length - 1].length === this.stackLimit) {
+        var newStack = []
+        newStack.push(val)
+        this.stackSet.push(newStack)
+    } else {
+        this.stackSet[this.stackSet.length - 1].push(val)
+    }
+}
+setOfStack.prototype.pop= function () {
+    if(this.stackSet[this.stackSet.length - 1].length === 0) {
+        this.stackSet.pop()
+    }
+    this.stackSet[this.stackSet.length - 1].pop()
+}
+setOfStack.prototype.popAt= function (index) {
+    this.stackSet[index].pop()
+}
+MyQueue = function {
+    var stack1 = []
+    var stack2 = []
+}
